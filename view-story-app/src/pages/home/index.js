853bb57ed5015e8c7e2453a55160a48d2dd30411
DaniloDuarte <danilojoseduarte@gmail.com>
2023-06-16 10:02:35 -0300

    Aplicando estilização a tela home

--- view-story-app/src/pages/home/index.js
+++ view-story-app/src/pages/home/index.js
@@ -1,9 +1,22 @@
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, TouchableOpacity, StatusBar, Platform } from 'react-native';
 
 export function Home(){
     return(
         <View style={styles.container}>
-            <Text>Página - Home</Text>
+            
+            <View style={styles.labels}>
+                <TouchableOpacity>
+                    <Text style={styles.labelsText}>Seguindo</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity>
+                    <Text style={styles.labelsText}>Pra Você
+                    </Text>
+                    <View style={styles.indicator}>
+                    </View>
+                </TouchableOpacity>
+            </View>
+
         </View>
     )
 }
@@ -12,5 +25,20 @@ const styles = StyleSheet.create({
     container:{
         flex: 1,
         backgroundColor: "#000",
+    },
+    labels:{
+        flexDirection:'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        gap: 8,
+        position: 'absolute',
+        top: 6,
+        left: 0,
+        right: 0,
+        marginTop: Platform.OS === 'android' ? StatusBar.currentHeight+5 : 55,
+        zIndex: 99,
+    },
+    labelsText:{
+        color: "#fff"
     }
 })
