97ffc47cb4c57e2cd802443024217b5cf07f8208
DaniloDuarte <danilojoseduarte@gmail.com>
2023-06-19 17:49:29 -0300

    Configurado tela para exibição através do heightScreen

--- view-story-app/src/components/FeedItem/index.js
+++ view-story-app/src/components/FeedItem/index.js
@@ -1,11 +1,19 @@
-import { View, Text, StyleSheet, Pressable } from "react-native";
-
+import { useRef, useRef, useState } from "react"
+import { View, Text, StyleSheet, Pressable, Dimensions } from "react-native";
 import {Video } from 'expo-av'
 
+const { height: heightScreen } = Dimensions.get("screen")
+
 export function FeedItem( {data} ) {
+    const video = useRef(null);
+
     return (
         <Pressable>
-            
+            <Video 
+                ref={video}
+                style={{width: '100%', height: heightScreen }}
+                source={{ uri:data?.video}}
+            />
         </Pressable>
     )
 }
