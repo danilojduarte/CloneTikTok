e62bd6db527e112a8af6eb6b9c058fe7591057fb
DaniloDuarte <danilojoseduarte@gmail.com>
2023-06-26 10:01:13 -0300

    Ajustando tela de exibição useStates

--- view-story-app/src/components/FeedItem/index.js
+++ view-story-app/src/components/FeedItem/index.js
@@ -6,14 +6,23 @@ const { height: heightScreen } = Dimensions.get("screen")
 
 export function FeedItem( {data} ) {
     const video = useRef(null);
+    const [status, setStatus] = useState({})
+
+    function handlePlayer(){
+        alert("teste")
+    }
 
     return (
-        <Pressable>
+        <Pressable onPress={handlePlayer}>
             <Video 
                 ref={video}
                 style={{width: '100%', height: heightScreen }}
                 source={{ uri:data?.video}}
                 resizeMode="cover"
+                shouldPlay={false}
+                isMuted={false}
+                isLooping
+                onPlaybackStatusUpdate={ status => setStatus(() => status)}
             />
         </Pressable>
     )
